b83f242c2adfd797a24af945dc12816c51154673
sylvain <sylvain@192.168.1.41>
2017-09-21 22:08:37 +0200

    Starting project

---
+++ ExampleFXProject/src/application/ControlledScreen.java
@@ -0,0 +1,8 @@
+package application;
+
+public interface ControlledScreen {
+	
+	// This method will allow the injection of the Parent screenPane
+	public void setScreenParent(ScreenController screenPage);
+
+}
